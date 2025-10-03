27cc6591bb748c2c9e6a1f7852808bc947110821
omer <omer@levys.co.il>
2025-10-04 00:45:57 +0300

    fonts fix

--- app-router/app/ui/fonts.ts
+++ app-router/app/ui/fonts.ts
@@ -1,4 +1,4 @@
-import { Inter } from 'next/font/google';
+import { Inter, Lusitana } from 'next/font/google';
  
 export const inter = Inter({ subsets: ['latin'] });
-export const secondary = Inter({weight: '400', subsets: ['lusitana'] });
+export const lusitana = Lusitana({weight: '400', subsets: ['latin'] });
