ddad78568a344784f3b37bfdb2d3b4cbfa433b47
omer <omer@levys.co.il>
2025-10-04 14:03:49 +0300

    fix

--- app-router/app/dashboard/(overview)/page.tsx
+++ app-router/app/dashboard/(overview)/page.tsx
@@ -7,7 +7,12 @@ import { fetchRevenue, fetchLatestInvoices, fetchCardData } from '@/app/lib/data
 export default async function Page() {
   const revenue = await fetchRevenue();
   const latestInvoices = await fetchLatestInvoices();
-  const {totalPaidInvoices, totalPendingInvoices, numberOfInvoices, numberOfCustomers} = await fetchCardData();
+  const {
+    totalPaidInvoices, 
+    totalPendingInvoices, 
+    numberOfInvoices, 
+    numberOfCustomers
+  } = await fetchCardData();
 
   return (
     <main>
